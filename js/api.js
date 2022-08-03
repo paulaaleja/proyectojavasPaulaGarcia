f42c2bdc3dc311c04e0d066b8612990bc67939ff
paula garcia <103803748+paulaaleja@users.noreply.github.com>
2022-08-02 23:02:09 -0300

    paulagarciaproyectojavascript

---
+++ js/api.js
@@ -0,0 +1,83 @@
+
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Actron 600 x 20 capsulas',
+        precio: 980,
+        imagen: 'https://i.ibb.co/sFv99F0/actron.jpg'
+    },
+    {
+        id: 2,
+        nombre: 'Aziatop x 10 capsulas',
+        precio: 540,
+        imagen: 'https://i.ibb.co/RbPF1zF/aziatop.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'Dulcolax x 20 grageas',
+        precio: 780,
+        imagen: 'https://i.ibb.co/25CrQ7X/dulcolaz.jpg'
+    },
+    {
+        id: 4,
+        nombre: 'Coltix gotas x 20ml',
+        precio: 965,
+        imagen: 'https://i.ibb.co/b2GqH38/coltix.jpg'
+
+    },
+    {
+        id: 5,
+        nombre: 'Alernix Spray nasal x 100ml',
+        precio: 1200,
+        imagen: 'https://i.ibb.co/0XSY8sm/alernix.jpg'
+
+    },
+    {
+        id: 6,
+        nombre: 'Curflex x 50 capsulas',
+        precio: 965,
+        imagen: 'https://i.ibb.co/wRyFpWQ/curflex.jpg'
+
+    },
+    {
+        id: 1,
+        nombre: 'Actron 600 x 20 capsulas',
+        precio: 980,
+        imagen: ''
+    },
+    {
+        id: 2,
+        nombre: 'Aziatop x 10 capsulas',
+        precio: 540,
+        imagen: ''
+    },
+    {
+        id: 3,
+        nombre: 'Dulcolax x 20 grageas',
+        precio: 780,
+        imagen: ''
+    },
+    {
+        id: 4,
+        nombre: 'Coltix gotas x 20ml',
+        precio: 965,
+        imagen: ''
+
+    },
+    {
+        id: 5,
+        nombre: 'Alernix Spray nasal x 100ml',
+        precio: 1200,
+        imagen: ''
+
+    },
+    {
+        id: 6,
+        nombre: 'Curflex x 50 capsulas',
+        precio: 965,
+        imagen: ''
+
+    }
+    
+
+];
